208aa2a9dd1b8c9c363e39ee11a41141a7e3235f
Minh Nguyen <minhnc.com@gmail.com>
2024-10-10 16:52:17 +1100

    Fix Frameworks/DyScan.framework/DyScan contains bitcode issue

--- plugin/src/with-dyscan-ios.ts
+++ plugin/src/with-dyscan-ios.ts
@@ -1,30 +1,49 @@
-import type { ConfigPlugin } from "@expo/config-plugins";
-import { withDangerousMod } from "@expo/config-plugins";
-import * as eol from "eol";
-import { readFileSync, writeFileSync } from "fs";
-import { resolve } from "path";
+import type { ConfigPlugin } from 'expo/config-plugins';
+import { withDangerousMod } from 'expo/config-plugins';
+import { readFileSync, writeFileSync } from 'fs';
+import { resolve } from 'path';
 
 const withDyScanPod: ConfigPlugin = (config) => {
   return withDangerousMod(config, [
-    "ios",
+    'ios',
     (cfg) => {
       const { platformProjectRoot } = cfg.modRequest;
-      const podfile = resolve(platformProjectRoot, "Podfile");
-      const contents = readFileSync(podfile, "utf-8");
+      const podfile = resolve(platformProjectRoot, 'Podfile');
+      let contents = readFileSync(podfile, 'utf-8');
 
-      const lines = eol.split(contents);
-      const index = lines.findIndex((line: string) =>
-        /\s+use_expo_modules!/.test(line),
+      const regUseExpo = /(\buse_native_modules!)/;
+      contents = contents.replace(
+        regUseExpo,
+        `$1\n
+  # Start-Adding DyScan.podspec
+  pod 'DyScan', :podspec => '../node_modules/@dyneti/react-native-dyscan/specs/DyScan.podspec'
+  # End-Adding DyScan.podspec`
       );
 
-      writeFileSync(
-        podfile,
-        [
-          ...lines.slice(0, index + 2),
-          `  pod 'DyScan', :podspec => '../node_modules/@dyneti/react-native-dyscan/specs/DyScan.podspec'`,
-          ...lines.slice(index + 2),
-        ].join("\n"),
+      const regPostInstall = /(\bpost_install do \|installer\|)/;
+      contents = contents.replace(
+        regPostInstall,
+        `$1
+    # Start-Stripping bitcode in DyScan
+    bitcode_strip_path = \`xcrun --find bitcode_strip\`.chop!
+    def strip_bitcode_from_framework(bitcode_strip_path, framework_relative_path)
+      framework_path = File.join(Dir.pwd, framework_relative_path)
+      command = "#{bitcode_strip_path} #{framework_path} -r -o #{framework_path}"
+      puts "Stripping bitcode: #{command}"
+      system(command)
+    end
+
+    framework_paths = [
+      "Pods/DyScan/Universal/DyScan.xcframework/ios-arm64/DyScan.framework/DyScan",
+    ]
+
+    framework_paths.each do |framework_relative_path|
+      strip_bitcode_from_framework(bitcode_strip_path, framework_relative_path)
+    end
+    # End-Stripping bitcode in DyScan\n`
       );
+
+      writeFileSync(podfile, contents);
 
       return cfg;
     },
